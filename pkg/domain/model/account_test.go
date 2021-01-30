21a828a77af773efd80b3c5a2dbeb2f2d351bd6a
Helder Alves <helder.jaspion@gmail.com>
2021-01-30 18:49:35 +0000

    test: account model tests

--- pkg/domain/model/account_test.go
+++ pkg/domain/model/account_test.go
@@ -2,7 +2,9 @@ package model
 
 import (
 	"golang.org/x/crypto/bcrypt"
+	"reflect"
 	"testing"
+	"time"
 )
 
 func TestAccount_HashSecret(t *testing.T) {
@@ -48,6 +50,52 @@ func TestAccount_HashSecret(t *testing.T) {
 
 			if err := bcrypt.CompareHashAndPassword([]byte(a.Secret), []byte(tt.fields.Secret)); err != nil {
 				t.Errorf("HashSecret() compareHashAndPassword, secret %v, error %v", a.Secret, err)
+			}
+		})
+	}
+}
+
+func TestNewAccount(t *testing.T) {
+	type args struct {
+		name    string
+		cpf     string
+		secret  string
+		balance float64
+	}
+	tests := []struct {
+		name string
+		args args
+		want *Account
+	}{
+		{
+			name: "",
+			args: args{
+				name:    "Bart Simpson",
+				cpf:     "12345678911",
+				secret:  "123456",
+				balance: 0,
+			},
+			want: &Account{
+				ID:        "",
+				Name:      "Bart Simpson",
+				CPF:       "12345678911",
+				Secret:    "123456",
+				Balance:   0,
+				CreatedAt: time.Time{},
+			},
+		},
+		// TODO add more tests (formatted cpf, negative balance, positive balance, empty fields)
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := NewAccount(tt.args.name, tt.args.cpf, tt.args.secret, tt.args.balance)
+			// TODO check if id was generated
+			// TODO check if createdAt was generated
+			got.ID = ""
+			got.CreatedAt = time.Time{}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewAccount() = %v, want %v", got, tt.want)
 			}
 		})
 	}
